a78d5daa9465ee3d46c4a2821bd34f28a82d1d20
陈蕾 <kevin_java_baseCode@kevin.com>
2020-08-20 09:56:55 +0800

    这是idea的git提交测试

---
+++ gitTest/src/com/kevin/test/test.java
@@ -0,0 +1,11 @@
+package com.kevin.test;
+
+/**
+ * @author Kevin-Lei-Chen
+ * @create 2020-08-20
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
